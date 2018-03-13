ac22404e85fc9565339666dbb36a881eb2566f68
Tomas Drąsutis <37337563+tomas-drasutis@users.noreply.github.com>
2018-03-13 16:25:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+             DrawPyramid(14);
+         }   
+         static void DrawPyramid(int n)
+         {
+             for (int i = 1; i <= n; i++)
+             {
+                 for (int j = i; j <= n; j++)
+                 {
+                     Console.Write("  ");
+                 }
+                 for (int k = 1; k <= 2 * i - 1; k++)
+                 {
+                     Console.Write("*"+ " ");
+                 }
+                 Console.WriteLine();
+             }
+         }
+
+
+    }
+
+}
